51b5adef8c877ed125b8b5698fa9ab519dd75b5e
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 18:45:38 +0530

    added url for filter pattern

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/BejC3S1SpringBootJwtPc1Application.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/BejC3S1SpringBootJwtPc1Application.java
@@ -1,13 +1,25 @@
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1;
 
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BejC3S1SpringBootJwtPc1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BejC3S1SpringBootJwtPc1Application.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean jwtFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new JwtFilter());
+        filterRegistrationBean.addUrlPatterns("api/v1/*");
+
+        return filterRegistrationBean;
+    }
 }
