21e00919354ba5b376d81d4b4edcc9fcc848120f
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 17:54:25 +0530

    implemented method to generate the JWT tocken

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/JwtSecurityTokenGenerator.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/JwtSecurityTokenGenerator.java
@@ -7,5 +7,25 @@
 
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service;
 
-public class JwtSecurityTokenGenerator {
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.domain.Customer;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtSecurityTokenGenerator implements SecurityTokenGenerator {
+    @Override
+    public Map<String, String> generateToken(Customer customer) {
+        String jwtToken = null;
+        jwtToken = Jwts.builder().setSubject(customer.getCustomerName()).setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("token", jwtToken);
+        map.put("message", "User Successfully logged in");
+        return map;
+    }
 }
+
