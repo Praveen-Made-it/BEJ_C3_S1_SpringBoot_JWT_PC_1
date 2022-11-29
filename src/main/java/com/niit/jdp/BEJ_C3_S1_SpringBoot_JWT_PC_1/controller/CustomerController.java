b0a7488688076ad0698fb2b1f708330cb1d32b86
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 18:04:57 +0530

    created constructor for ICustomerService and SecurityTokenGenerator class

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
@@ -8,8 +8,14 @@
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.controller;
 
 import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.ICustomerService;
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.SecurityTokenGenerator;
 
 public class CustomerController {
-    private ICustomerService iCustomerService;
+    private ICustomerService customerService;
+    private SecurityTokenGenerator securityTokenGenerator;
 
+    public CustomerController(ICustomerService customerService, SecurityTokenGenerator securityTokenGenerator) {
+        this.customerService = customerService;
+        this.securityTokenGenerator = securityTokenGenerator;
+    }
 }
