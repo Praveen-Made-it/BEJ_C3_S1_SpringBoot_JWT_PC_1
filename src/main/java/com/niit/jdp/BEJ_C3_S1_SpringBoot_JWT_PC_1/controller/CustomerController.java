9f9efe9af6db002203ecef086be47901a48ba7d8
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 18:39:12 +0530

    created DeleteMapping For delete customers from the database

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
@@ -15,10 +15,7 @@ import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.SecurityTokenGenerator
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -60,5 +57,17 @@ public class CustomerController {
         return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);
     }
 
+    @DeleteMapping("/api/v1/delete/{customerId}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable int customerId) throws CustomerNotFoundException {
+        ResponseEntity responseEntity = null;
+        try {
+            customerService.deleteCustomer(customerId);
+            responseEntity = new ResponseEntity<>("successfully deleted one record", HttpStatus.OK);
+        } catch (CustomerNotFoundException e) {
+            throw new CustomerNotFoundException();
+        } catch (Exception e) {
+            responseEntity = new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
+    }
 }
-
