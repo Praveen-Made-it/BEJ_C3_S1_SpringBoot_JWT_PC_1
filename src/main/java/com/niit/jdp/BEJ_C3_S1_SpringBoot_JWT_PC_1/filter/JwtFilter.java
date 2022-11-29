838f92b6c9cfdaf6bb495a51be8322bfd5c858d1
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 17:56:32 +0530

    implemented the methods from genericFilterBean

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/filter/JwtFilter.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/filter/JwtFilter.java
@@ -7,5 +7,17 @@
 
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.filter;
 
-public class JwtFilter {
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class JwtFilter extends GenericFilterBean {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
 }
