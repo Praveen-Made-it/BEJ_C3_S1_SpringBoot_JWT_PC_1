92f4880c3a5f298672bd515efe16634fe45a558c
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 16:50:37 +0530

    added @ResponseStatus annotation and added user defined response message/reason

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/exception/CustomerNotFoundException.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/exception/CustomerNotFoundException.java
@@ -7,5 +7,9 @@
 
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Customer Not Found in the Database")
 public class CustomerNotFoundException extends Exception {
 }
