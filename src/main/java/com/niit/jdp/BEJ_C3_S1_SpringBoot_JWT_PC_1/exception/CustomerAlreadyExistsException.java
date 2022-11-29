0b6c3bf970582019fcbdddacbe6edb2512435d17
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 16:47:11 +0530

    added @ResponseStatus annotation and added user defined response message/reason

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/exception/CustomerAlreadyExistsException.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/exception/CustomerAlreadyExistsException.java
@@ -7,5 +7,9 @@
 
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "The Customer is already Exist in the Database")
 public class CustomerAlreadyExistsException extends Exception {
 }
