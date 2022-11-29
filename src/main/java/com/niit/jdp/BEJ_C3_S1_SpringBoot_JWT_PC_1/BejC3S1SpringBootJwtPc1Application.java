501c2d4a7bb150a00443d05a4d301c2fc60f952c
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 16:01:20 +0530

    Initial commit

---
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/BejC3S1SpringBootJwtPc1Application.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejC3S1SpringBootJwtPc1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BejC3S1SpringBootJwtPc1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/BejC3S1SpringBootJwtPc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejC3S1SpringBootJwtPc1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
