f2a3c58d977b5e4e5eef86ed50c4cbf1708e91e8
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 16:22:48 +0530

    created getAllCustomers method to fetch the customer details from the database

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/ICustomerService.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/ICustomerService.java
@@ -2,9 +2,13 @@ package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service;
 
 import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.domain.Customer;
 
+import java.util.List;
+
 public interface ICustomerService {
 
     public Customer saveCustomer(Customer customer);
+
+    public List<Customer> getAllCustomers();
 
 
 }
