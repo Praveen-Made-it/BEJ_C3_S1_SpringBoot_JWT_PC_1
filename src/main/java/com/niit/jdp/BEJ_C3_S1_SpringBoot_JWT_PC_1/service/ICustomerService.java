62032286019f6d59e280e2628b16b82a898bc8ae
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 16:20:59 +0530

    created saveCustomer method to save the customer details in the database

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/ICustomerService.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/ICustomerService.java
@@ -1,4 +1,10 @@
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service;
 
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.domain.Customer;
+
 public interface ICustomerService {
+
+    public Customer saveCustomer(Customer customer);
+
+
 }
