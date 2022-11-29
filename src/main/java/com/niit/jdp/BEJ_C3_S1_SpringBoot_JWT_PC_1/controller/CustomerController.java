65b7e29052db9b0636add6d19f64f7062f8fbdb2
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 18:14:20 +0530

    created PostMapping For register

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
@@ -7,9 +7,15 @@
 
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.controller;
 
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.domain.Customer;
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.exception.CustomerAlreadyExistsException;
 import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.ICustomerService;
 import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.SecurityTokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +27,13 @@ public class CustomerController {
     public CustomerController(ICustomerService customerService, SecurityTokenGenerator securityTokenGenerator) {
         this.customerService = customerService;
         this.securityTokenGenerator = securityTokenGenerator;
-
-
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> savefunction(@RequestBody Customer customer) throws CustomerAlreadyExistsException {
+        return new ResponseEntity<>(customerService.saveCustomer(customer), HttpStatus.CREATED);
+    }
+
+
 }
+
