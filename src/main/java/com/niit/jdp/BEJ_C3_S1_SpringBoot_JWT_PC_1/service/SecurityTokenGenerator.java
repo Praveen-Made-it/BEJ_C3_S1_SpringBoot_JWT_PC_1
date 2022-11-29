b292d2816665972f9b29d2c341e5527c2a415f7e
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 17:48:21 +0530

    created generateToken method

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/SecurityTokenGenerator.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/service/SecurityTokenGenerator.java
@@ -1,4 +1,9 @@
 package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service;
 
+import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.domain.Customer;
+
+import java.util.Map;
+
 public interface SecurityTokenGenerator {
+    Map<String, String> generateToken(Customer customer);
 }
