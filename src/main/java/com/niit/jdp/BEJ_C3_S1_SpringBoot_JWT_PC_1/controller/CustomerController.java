de7cea55f68dabcfa22c6122dfc31d73f1e8f39a
Praveen Kumar <praveen170596@gmail.com>
2022-11-29 18:08:26 +0530

    @RestController Annotation for CustomerController class

--- src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
+++ src/main/java/com/niit/jdp/BEJ_C3_S1_SpringBoot_JWT_PC_1/controller/CustomerController.java
@@ -9,7 +9,9 @@ package com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.controller;
 
 import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.ICustomerService;
 import com.niit.jdp.BEJ_C3_S1_SpringBoot_JWT_PC_1.service.SecurityTokenGenerator;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CustomerController {
     private ICustomerService customerService;
     private SecurityTokenGenerator securityTokenGenerator;
@@ -17,5 +19,7 @@ public class CustomerController {
     public CustomerController(ICustomerService customerService, SecurityTokenGenerator securityTokenGenerator) {
         this.customerService = customerService;
         this.securityTokenGenerator = securityTokenGenerator;
+
+
     }
 }
